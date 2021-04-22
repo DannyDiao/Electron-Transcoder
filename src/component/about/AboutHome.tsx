a976fde93c9ff61657d504805bad7caeced6e1b2
diaosu <diaosu@diaosudev.cn>
2021-04-23 01:19:36 +0800

    添加关于页面返回主页按钮

--- src/component/about/AboutHome.tsx
+++ src/component/about/AboutHome.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Fab } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import {Link} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class AboutHome extends React.Component {
-  render() {
-    return <h1>AboutHome!</h1>;
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent:'flex-end'
   }
+}));
+
+export default function AboutHome(){
+  const classes = useStyles();
+    return (
+      <div className={classes.container}>
+        <h1>AboutHome!</h1>
+        <Fab component={Link} to="/" color="primary" aria-label="add">
+          <HomeIcon />
+        </Fab>
+      </div>
+    );
 }
